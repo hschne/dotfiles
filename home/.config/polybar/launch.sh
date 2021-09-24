34e28fd57919430b7291875d5ba9ce3255afce6e
hschne <hans.schnedlitz@gmail.com>
2021-09-24 10:07:21 +0200

    Improve polybar wlan widget

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+DEFAULT_NETWORK_INTERFACE=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
+
 MONITOR1="$(xrandr --listmonitors | grep "0:" | cut -d ' ' -f6)"
 MONITOR2="$(xrandr --listmonitors | grep "1:" | cut -d ' ' -f6)"
 
