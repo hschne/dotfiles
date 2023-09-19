247a796921e9c6ab3905155f57c626e71c04319d
hschne <hans.schnedlitz@gmail.com>
2023-09-19 10:33:19 +0200

    Add vim end wise

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,8 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       indent = { enable = false }, -- Indent with Treesitter is fucked, use classic Vim instead
+      endwise = { enable = true },
     },
   },
+  { "RRethy/nvim-treesitter-endwise", dependencies = { "nvim-treesitter/nvim-treesitter" } },
 }
