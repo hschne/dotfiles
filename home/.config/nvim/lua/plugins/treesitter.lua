112358ee7fac1d0c3a689428cac4617888eb297d
hschne <hans.schnedlitz@gmail.com>
2024-03-02 13:41:50 +0100

    Add aerial

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,16 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "RRethy/nvim-treesitter-endwise"
-    },
-    opts = {
-      indent = { enable = false }, -- Indent with Treesitter is fucked, use classic Vim instead
-      endwise = { enable = true },
-    },
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "scss",
+      })
+    end,
     config = function()
       local opt = vim.opt
       opt.foldmethod = "expr"
       opt.foldexpr = "nvim_treesitter#foldexpr()"
       opt.foldlevel = 99
-    end
+    end,
   },
 }
