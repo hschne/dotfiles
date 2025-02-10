5b18c2e111f518ac7f9d4bb0fde93af4a11cc278
hschne <hello@hansschnedlitz.com>
2025-02-10 16:03:14 +0100

    More lazy fixes

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -10,6 +10,10 @@ return {
           mason = false,
           cmd = { vim.fn.expand("~/.local/share/mise/shims/ruby-lsp") },
         },
+        rubocop = {
+          mason = false,
+          cmd = { "bundle", "exec", "rubocop", "--lsp" },
+        },
       },
     },
   },
