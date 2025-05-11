7c4b3dbd8ac3f80ecac599f621242e6fcd47516e
hschne <hello@hansschnedlitz.com>
2025-05-11 10:22:19 +0200

    Mason workaround

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -1,6 +1,9 @@
 return {
   {
     "williamboman/mason.nvim",
+    -- Pin version for workaroud
+    -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "bash-language-server",
@@ -19,4 +22,5 @@ return {
       },
     },
   },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
