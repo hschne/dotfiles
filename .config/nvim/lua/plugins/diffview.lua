efb6ef64f3ae070798aef09ef9cb1d8247afa753
hschne <hello@hansschnedlitz.com>
2026-07-09 12:41:54 +0200

    Add diffview

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory",
+    },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+      { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+    },
+    opts = {},
+  },
+}
