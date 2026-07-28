2930b0a513f794a084b60296e5058e872a74d447
hschne <hello@hansschnedlitz.com>
2026-07-28 08:12:16 +0200

    Add mattermost

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -475,6 +475,7 @@ hl.window_rule({ match = { title = "^(Spotify.*)$" }, workspace = "5" })
 hl.window_rule({ match = { class = "^([Ss]lack.*)$" }, workspace = "6" })
 hl.window_rule({ match = { class = "^([Ss]ignal.*)$" }, workspace = "6" })
 hl.window_rule({ match = { class = "^([Dd]iscord.*)$" }, workspace = "6" })
+hl.window_rule({ match = { class = "^([Mm]attermost.*)$" }, workspace = "6" })
 
 -- Float certain windows
 hl.window_rule({ match = { class = "^(org.speedcrunch.speedcrunch)$" }, float = true })
